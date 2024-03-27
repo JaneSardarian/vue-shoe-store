e9781e820e1d98661e06348b5b2985e458975377
Jane Sardarian <sardarianjane@gmail.com>
2024-03-27 14:18:11 +0400

    I prepared the Vue + Vite template with TailwindCSS and created the Header

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="flex justify-between border-b border-slate-300 px-10 py-8">
+    <div class="flex items-center gap-4">
+      <img src="/logo.png" alt="Logo" class="w-10" />
+      <div>
+        <h2 class="text-xl font-bold uppercase">Sneakers</h2>
+        <p class="text-slate-400">The best sneakers store</p>
+      </div>
+    </div>
+
+    <ul class="flex items-center gap-10">
+      <li class="flex items-center gap-2 text-gray-500 hover:text-black cursor-pointer">
+        <img src="/cart.svg" alt="Cart" />
+        <span>$ 120</span>
+      </li>
+      <li class="flex items-center gap-2 text-gray-500 hover:text-black cursor-pointer">
+        <img src="/heart.svg" alt="Favorites" />
+        <span>Favorites</span>
+      </li>
+      <li class="flex items-center gap-2 text-gray-500 hover:text-black cursor-pointer">
+        <img src="/profile.svg" alt="Profile" />
+        <span>Profile</span>
+      </li>
+    </ul>
+  </header>
+</template>
---
+++ src/components/MyHello.vue
@@ -0,0 +1,62 @@
+<!-- Это Api options -->
+
+<!-- <script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    }
+  }
+} -->
+<!-- </script> -->
+
+<!-- А это Api Composition -->
+<!-- <script setup>
+import { ref } from 'vue'
+let count = ref(0)
+
+function increment() {
+  count.value++
+} -->
+
+<!-- // function updateCount(event) {
+//   count.value = Number(event.target.value)
+// } -->
+<!-- </script> -->
+
+<!-- <template>
+  <h1>{{ count }}</h1>
+  <button @click="increment">+</button>
+  <input @keyup.ctrl.enter="increment" />
+</template> -->
+
+<!-- <script setup>
+const props = defineProps({
+  title: String,
+  price: Number,
+  isAdded: Boolean
+})
+
+console.log(props)
+</script>
+<template>
+  <div class="product">
+    <h2>{{ title }}</h2>
+    <h4>{{ price }}</h4>
+    <p v-if="isAdded">Added</p>
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.product {
+  border: 1px solid black;
+  padding: 20px;
+  margin-bottom: 10px;
+}
+</style> -->
